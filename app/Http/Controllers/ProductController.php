1d17778e360760db7ed3ca833ddb7c3e5afa9eeb
Favizille <favourgabrirl50@gmail.com>
2024-03-25 21:38:44 +0100

    uploading project with Product model, migrations, controller and the product CRUD functionalities

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+
+class ProductController extends Controller
+{
+    public function __construct(protected Product $product){}
+
+    public function createProduct(ProductRequest $request){
+         $request->validated();
+
+        $productDetails = [
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+            'quanity' => $request->input('quanity'),
+        ];
+
+        if(!$this->product->create($productDetails)){
+            return [
+                'status' => 'failed',
+                'status_code' => 400,
+                'message' => 'Product creation failed'
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'status_code' => 200,
+            'message' => 'product product was not created'
+        ];
+    }
+
+
+    public function getProduct($productId){
+        if(!$product = $this->product->find($productId)){
+            return [
+                'status' => 'failed',
+                'status_code' => 404,
+                'message' => 'Product not found'
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => $product
+        ];
+    }
+
+    public function updateProduct($productId, Request $request){
+        if(!$product = $this->product->find($productId)){
+            return [
+                'status' =>'failed',
+                'status_code' => 404,
+                'message' => 'Product not found',
+            ];
+        }
+
+        $productDetails = [
+            'title'=> $request->title,
+            'body'=> $request->body,
+        ];
+
+
+        if(!$product->update($productDetails)){
+            return [
+                'status' =>'failed',
+                'status_code' => 400,
+                'message' => 'Failed to update product',
+            ];
+        }
+
+        return [
+            'status' =>'success',
+            'status_code' => 200,
+            'data' => $product,
+        ];
+    }
+
+    public function deleteProduct($productId){
+        $product= $this->product->find($productId);
+
+        if(!$product){
+            return [
+                'status' =>'failed',
+                'message' => 'Could not find product',
+            ];
+        }
+
+        if(!$product->delete()){
+            return [   
+                'status' =>'failed',
+                'message' => 'Could not delete product',
+            ];
+        }
+
+        return [
+            'status' =>'success',
+            'status_code' => 200,
+            'message' => 'product Deleted',
+        ];
+    }
+}
